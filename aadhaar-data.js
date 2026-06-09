329c931ef003e993dc7b2a1442f84b923ee5b2a2
chibpratham48 <chibpratham48@users.noreply.github.com>
2026-06-09 07:53:49 +0530

    added space

--- aadhaar-data.js
+++ aadhaar-data.js
@@ -22,5 +22,54 @@ module.exports = [
       address: "Delhi, India",
       state: "Delhi"
     }
+  },
+  {
+    aadhaar: "482716397504",
+    vid: "9876543210987654",
+    mobile: "+918045653062",
+    details: {
+      name: "Venkatesh Sakkaji",
+      dob: "12-03-1998",
+      gender: "Male",
+      address: "Delhi, India",
+      state: "Delhi"
+    }
+  },
+  {
+    aadhaar: "918260473518",
+    vid: "9876543210987654",
+    mobile: "+919840514416",
+    details: {
+      name: "Venkatesh Sakkaji",
+      dob: "12-03-1998",
+      gender: "Male",
+      address: "Delhi, India",
+      state: "Delhi"
+    }
+  },
+  {
+    aadhaar: "736428519407",
+    vid: "9876543210987654",
+    mobile: "+918045653384",
+    details: {
+      name: "Raja Gupta",
+      dob: "12-03-1998",
+      gender: "Male",
+      address: "Delhi, India",
+      state: "Delhi"
+    }
+  },
+  {
+    aadhaar: "560387412965",
+    vid: "9876543210987654",
+    mobile: "+917032657756",
+    details: {
+      name: "Raja Gupta",
+      dob: "12-03-1998",
+      gender: "Male",
+      address: "Delhi, India",
+      state: "Delhi"
+    }
   }
+
 ];
